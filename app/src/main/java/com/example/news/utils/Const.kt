a89b201f6ad24bc68ed564cf648a95683bb6eed9
Abdullah Bendary <141359699+Abdullah-BenDarY@users.noreply.github.com>
2024-09-26 21:03:23 +0300

    Initial commit

---
+++ app/src/main/java/com/example/news/utils/Const.kt
@@ -0,0 +1,23 @@
+package com.example.news.utils
+
+const val SPORT_CATEGORY = "sports"
+const val TECHNOLOGY_CATEGORY = "technology"
+const val SCIENCE_CATEGORY = "science"
+const val HEALTH_CATEGORY = "health"
+const val GENERAL_CATEGORY = "general"
+const val ENTERTAINMENT_CATEGORY = "entertainment"
+const val BUSINESS_CATEGORY = "business"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/news/utils/Extentions.kt
@@ -0,0 +1,41 @@
+package com.example.news.utils
+
+import android.content.Context
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AlertDialog
+import com.example.news.R
+
+
+fun Context.showDialog(
+    @StringRes messageId: Int? = null,
+    message: String? = null,
+    @StringRes posBtnTextId: Int = R.string.ok,
+    @StringRes negBtnTextId: Int? = null,
+
+    onPositiveClick: (() -> Unit)? = null,
+    onNegativeClick: (() -> Unit)? = null,
+    isCancelable: Boolean = true
+) {
+    val builder = AlertDialog.Builder(this)
+    if (messageId != null) builder.setMessage(getString(messageId))
+    message?.let { builder.setMessage(it) }
+    builder.setPositiveButton(getString(posBtnTextId)) { dialog, i ->
+        onPositiveClick?.invoke()
+        dialog.dismiss()
+    }
+    if (negBtnTextId != null) {
+        builder.setNegativeButton(getString(negBtnTextId)) { _, _ ->
+            onNegativeClick?.invoke()
+        }
+    }
+
+    builder.setCancelable(isCancelable)
+    builder.create().show()
+}
+
+//fun <T> String.fromJson(className: Class<T>): T {
+//    return Gson().fromJson(this, className)
+//}
+
+
+
